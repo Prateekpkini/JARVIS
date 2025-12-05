670adf9d4cfc1f8c5c4ead46aa03c2ca4809d058
Prateekpkini <Prateekpkini2005@gmail.com>
2025-12-05 12:12:47 +0530

    Updated the requirements

--- JARVIS_MARK_21_Beta/api/gemini.py
+++ JARVIS_MARK_21_Beta/api/gemini.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
-
+GEMINI_API_KEY="AIzaSyBirC9uwxDdkZxHwuG_S2sJQojkXalHyKg"
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
